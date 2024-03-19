087666f126cef30a584c0880210bcdd37af99234
Damien Debin <damien.debin@gmail.com>
2024-03-19 15:45:29 +0100

    Adds test for PHP 8.3.

--- example/feed.php
+++ example/feed.php
@@ -2,19 +2,22 @@
 
 declare(strict_types = 1);
 
+use AtomGenerator\Entry;
+use AtomGenerator\Feed;
+
 include_once 'vendor/autoload.php';
 
-$entry = new AtomGenerator\Entry();
+$entry = new Entry();
 $entry->setTitle('Post', 'text');
 $entry->setId('tag:id');
 $entry->setContent('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'text');
 $entry->setUpdatedDateTime(new DateTime('2019-05-04T21:00:40Z'));
 
-$feed = new AtomGenerator\Feed();
+$feed = new Feed();
 $feed->setTitle('Blog');
 $feed->setUpdatedDateTime(new DateTime('now'));
 $feed->addEntry($entry);
 
-assert(AtomGenerator\Feed::validate($feed->getDocument()));
+assert(Feed::validate($feed->getDocument()));
 
 echo $feed->saveXML();
